d8ca6f5b3d645b2df092d30d72d0a580a4301d1e
Ammarbadawi18 <ammarbadawi18@gmail.com>
2023-12-03 14:04:35 +0200

    Project setup

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+import {Container, Navbar} from "react-bootstrap";
+
+export function NavBar () { 
+    return (
+        <Navbar>
+            <Container>
+                    Nav
+            </Container>
+        </Navbar>
+    )
+}
